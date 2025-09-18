480f19c09c5a9a3feb091fa980eb3ac3ef95ed75
Peter Kester <peter.kester@solace.com>
2025-09-18 09:50:59 +0200

    renamed the target image
    added root project as volume on the Solace Agent Mesh

--- prepare-image.sh
+++ prepare-image.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker load -i solace-agent-mesh-enterprise-1.0.37.tar | awk '{print $3}' | xargs -I {} docker tag {} my-solace/solace-agent-mesh-enterprise:1.0.37
+docker load -i solace-agent-mesh-enterprise-1.0.37.tar | awk '{print $3}' | xargs -I {} docker tag {} solace/solace-agent-mesh-enterprise:1.0.37
